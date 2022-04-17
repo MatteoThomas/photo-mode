8de9ef29a42878410893da34159049b996843568
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2022-04-17 13:55:14 -0600

    changed baseURL and way its used

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
-// const API_URL = "https://photo-mode.herokuapp.com/api/auth/";
+// const API_URL = "http://localhost:8080/api/auth/";
+// const baseURL = "https://photo-mode.herokuapp.com/api/auth/";
+const API = axios.create({
+  baseURL: "https://photo-mode.herokuapp.com/api/auth/",
+});
 
 const register = (username, email, password) => {
-  return axios.post(API_URL + "signup", {
+  return API.post(URL + "signup", {
     username,
     email,
     password,
@@ -12,30 +15,26 @@ const register = (username, email, password) => {
 };
 
 const login = (username, password) => {
-  return axios
-    .post(API_URL + "signin", {
-      username,
-      password,
-    })
-    .then((response) => {
-      if (response.data.accessToken) {
-        localStorage.setItem("user", JSON.stringify(response.data));
-      }
-      return response.data;
-    });
+  return API.post(URL + "signin", {
+    username,
+    password,
+  }).then((response) => {
+    if (response.data.accessToken) {
+      localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
+  });
 };
 
 const editBio = (bio, nameData, emailData) => {
-  return axios
-    .post(API_URL + "editBio", {
-      bio: bio,
-      username: nameData,
-      email: emailData,
-    })
-    .then((response) => {
-      console.log(response.data.bio);
-      return response.data.bio;
-    });
+  return API.post(URL + "editBio", {
+    bio: bio,
+    username: nameData,
+    email: emailData,
+  }).then((response) => {
+    console.log(response.data.bio);
+    return response.data.bio;
+  });
 };
 
 const authService = {
